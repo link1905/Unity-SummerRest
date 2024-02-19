2de9b990663105a87a6987c404d16be631f85a0c
risethesummer <honhatlinhmail@gmail.com>
2024-02-19 13:23:51 +0700

    fix to use command line args instead of method params in Export

--- SummerRest/Assets/Editor/ExportPackage.cs
+++ SummerRest/Assets/Editor/ExportPackage.cs
@@ -1,14 +1,23 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 
 namespace Editor
 {
     public static class ExportPackage
     {
-        public static void Export (string packagePath, string outputPath) {
+        private static readonly string Eol = Environment.NewLine;
+        private static readonly string[] Needs = {"packagePath", "outputPath"};
+        public static void Export () {
+            var validatedOptions = ParseCommandLineArguments();
+            var packagePath = validatedOptions["packagePath"];
+            var outputPath = validatedOptions["outputPath"];
             // Ensure export path.
             var dir = new FileInfo(outputPath).Directory;
             if (dir is { Exists: false }) {
+                Console.WriteLine($"The directory of {outputPath} does not exist => automatically create a new one");
                 dir.Create();
             }
             // Export
@@ -17,6 +26,45 @@ namespace Editor
                 outputPath,
                 ExportPackageOptions.Recurse | ExportPackageOptions.IncludeDependencies
             );
+        }
+        private static Dictionary<string, string> ParseCommandLineArguments()
+        {
+            var providedArguments = new Dictionary<string, string>();
+            var args = Environment.GetCommandLineArgs();
+            Console.WriteLine(
+                $"{Eol}" +
+                $"###########################{Eol}" +
+                $"#    Parsing settings     #{Eol}" +
+                $"###########################{Eol}" +
+                $"{Eol}"
+            );
+            // Extract flags with optional values
+            for (int current = 0, next = 1; current < args.Length; current++, next++)
+            {
+                // Parse flag
+                bool isFlag = args[current].StartsWith("-");
+                if (!isFlag) 
+                    continue;
+                var flag = args[current].TrimStart('-');
+                // Parse optional value
+                var flagHasValue = next < args.Length && !args[next].StartsWith("-");
+                var value = flagHasValue ? args[next].TrimStart('-') : "";
+                if (!Needs.Contains(flag))
+                    continue;
+                // Assign
+                Console.WriteLine($"Found flag \"{flag}\" with value \"{value}\".");
+                providedArguments.Add(flag, value);
+            }
+
+            foreach (var need in Needs)
+            {
+                if (!providedArguments.TryGetValue(need, out var val) || string.IsNullOrEmpty(val))
+                {
+                    Console.WriteLine($"Missing argument -{need}");
+                    EditorApplication.Exit(110);
+                }
+            }
+            return providedArguments;
         }
     }
 }
